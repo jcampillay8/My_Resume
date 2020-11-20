a76cb4264f9eda217a0d7dc14e9a163b084bdb6a
Jaime Campillay <jgcampillayworks@gmail.com>
2020-11-20 13:05:39 -0300

    Funciona todo bien sin heroku aún

--- djangoresume/djangoresume/settings.py
+++ djangoresume/djangoresume/settings.py
@@ -1,8 +1,8 @@
 
-from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -66,7 +66,7 @@ WSGI_APPLICATION = 'djangoresume.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -108,3 +108,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'C:/djangoresume/Static'),
+]
